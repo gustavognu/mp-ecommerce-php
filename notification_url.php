65d7bf5facb9ad1487ab45e0c54b35e53dcdf7e6
gus <gustavomartinez@gmail.com>
2022-03-14 13:04:48 -0600

    test

--- notification_url.php
+++ notification_url.php
@@ -1,6 +1,5 @@
 <?php
 
 $body = @file_get_contents('php://input');
-$data = json_decode($body, true);
 
-file_put_contents("eventos.json", json_encode($_POST));
+file_put_contents("eventos.json", $body);
