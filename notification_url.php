d6d72da3ba255afb298ad11f796617c11b3aa65f
gus <gustavomartinez@gmail.com>
2022-03-14 13:22:01 -0600

    test

--- notification_url.php
+++ notification_url.php
@@ -1,5 +1,7 @@
 <?php
 
-// $body = @file_get_contents('php://input');
+$body = @file_get_contents('php://input');
 
-file_put_contents("eventos.json", $_POST);
+file_put_contents("eventos.json",date("H:i:s")." > ". json_encode($_POST) . "\n", FILE_APPEND);
+file_put_contents("eventos.json",date("H:i:s")." > ". json_encode($_REQUEST) . "\n", FILE_APPEND);
+file_put_contents("eventos.json",date("H:i:s")." > ". json_encode($body) . "\n", FILE_APPEND);
