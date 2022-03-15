b718a3f3dcd4f7317ab87910992a385eb8e278fb
gus <gustavomartinez@gmail.com>
2022-03-15 16:08:44 -0600

    test

--- detail.php
+++ detail.php
@@ -20,6 +20,8 @@
     $result= json_decode($result, true);
     $access_token= $result['access_token'];
 
+    // echo $access_token; return;
+
     $data_body= '{
             "items": [{
                 "id": "1234",
